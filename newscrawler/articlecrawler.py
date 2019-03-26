50e64c85bd2a16caf8bc7024a202b5eff65b9f5a
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2019-03-26 19:08:19 +0900

    Edit Date

--- newscrawler/articlecrawler.py
+++ newscrawler/articlecrawler.py
@@ -141,5 +141,5 @@ class ArticleCrawler(object):
 if __name__ == "__main__":
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학", "생활문화")  # 정치, 경제, 생활문화, IT과학, 사회 카테고리 사용 가능
-    Crawler.set_date_range(2018, 2018, 1)  # 2017년 1월부터 2018년 4월까지 크롤링 시작
+    Crawler.set_date_range(2017, 2018, 4)  # 2017년 1월부터 2018년 4월까지 크롤링 시작
     Crawler.start()
