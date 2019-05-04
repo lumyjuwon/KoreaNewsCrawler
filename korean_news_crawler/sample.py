673e15c307ec6d6ad23f1e27380b3f921683c137
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2019-05-04 16:45:48 +0900

    Change sample date range

--- korean_news_crawler/sample.py
+++ korean_news_crawler/sample.py
@@ -3,5 +3,5 @@ from korean_news_crawler.articlecrawler import ArticleCrawler
 if __name__ == "__main__":
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학", "경제")  # 정치, 경제, 생활문화, IT과학, 사회 카테고리 사용 가능
-    Crawler.set_date_range(2018, 2018, 1)  # 2017년 1월부터 2018년 4월까지 크롤링 시작
+    Crawler.set_date_range(2017, 2018, 3)  # 2017년 1월부터 2018년 3월까지 크롤링 시작
     Crawler.start()
