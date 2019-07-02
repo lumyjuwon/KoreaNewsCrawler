1fac5257fba66e9d139dad3d4a250cdd775daca8
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2019-07-03 00:18:04 +0900

    Change start month and end month
    주석에 맞게 파라미터 값을 수정함

--- korea_news_crawler/sample.py
+++ korea_news_crawler/sample.py
@@ -3,5 +3,5 @@ from korea_news_crawler.articlecrawler import ArticleCrawler
 if __name__ == "__main__":
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학", "경제")  # 정치, 경제, 생활문화, IT과학, 사회, 세계 카테고리 사용 가능
-    Crawler.set_date_range(2017, 1, 2017, 1)  # 2017년 1월부터 2018년 3월까지 크롤링 시작
+    Crawler.set_date_range(2017, 7, 2018, 3)  # 2017년 1월부터 2018년 3월까지 크롤링 시작
     Crawler.start()
