1740d09a20ea5bd850ec600794b8a690d87afba0
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2020-12-09 23:51:27 +0900

    Refactoring
    change variable name and delete not used member variable
    move comment position

--- korea_news_crawler/articlecrawler.py
+++ korea_news_crawler/articlecrawler.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8, euc-kr -*-
 
-from time import sleep
-from bs4 import BeautifulSoup
-from multiprocessing import Process
-#from korea_news_crawler.exceptions import *
-from exceptions import *
-from articleparser import ArticleParser
-from writer import Writer
-#from korea_news_crawler.articleparser import ArticleParser
-#from korea_news_crawler.writer import Writer
 import os
 import platform
 import calendar
 import requests
 import re
+from time import sleep
+from bs4 import BeautifulSoup
+from multiprocessing import Process
+from korea_news_crawler.exceptions import *
+from korea_news_crawler.articleparser import ArticleParser
+from korea_news_crawler.writer import Writer
 
 
 class ArticleCrawler(object):
@@ -49,21 +46,21 @@ class ArticleCrawler(object):
     def make_news_page_url(category_url, start_year, end_year, start_month, end_month):
         made_urls = []
         for year in range(start_year, end_year + 1):
-            if start_year == end_year:
-                year_startmonth = start_month
-                year_endmonth = end_month
-            else:
+            target_start_month = start_month
+            target_end_month = end_month
+
+            if start_year != end_year:
                 if year == start_year:
-                    year_startmonth = start_month
-                    year_endmonth = 12
+                    target_start_month = start_month
+                    target_end_month = 12
                 elif year == end_year:
-                    year_startmonth = 1
-                    year_endmonth = end_month
+                    target_start_month = 1
+                    target_end_month = end_month
                 else:
-                    year_startmonth = 1
-                    year_endmonth = 12
+                    target_start_month = 1
+                    target_end_month = 12
             
-            for month in range(year_startmonth, year_endmonth + 1):
+            for month in range(target_start_month, target_end_month + 1):
                 for month_day in range(1, calendar.monthrange(year, month)[1] + 1):
                     if len(str(month)) == 1:
                         month = "0" + str(month)
@@ -75,20 +72,19 @@ class ArticleCrawler(object):
 
                     # totalpage는 네이버 페이지 구조를 이용해서 page=10000으로 지정해 totalpage를 알아냄
                     # page=10000을 입력할 경우 페이지가 존재하지 않기 때문에 page=totalpage로 이동 됨 (Redirect)
-
                     totalpage = ArticleParser.find_news_totalpage(url + "&page=10000")
                     for page in range(1, totalpage + 1):
                         made_urls.append(url + "&page=" + str(page))
         return made_urls
 
     @staticmethod
-    def get_url_data(url, max_tries=10):
+    def get_url_data(url, max_tries=5):
         remaining_tries = int(max_tries)
         while remaining_tries > 0:
             try:
-                return requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
+                return requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
             except requests.exceptions:
-                sleep(60)
+                sleep(1)
             remaining_tries = remaining_tries - 1
         raise ResponseTimeout()
 
@@ -96,34 +92,32 @@ class ArticleCrawler(object):
         # Multi Process PID
         print(category_name + " PID: " + str(os.getpid()))    
 
-        writer = Writer(category_name=category_name, date=self.date)
-        # 기사 URL 형식
-        url = "http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=" + str(self.categories.get(category_name)) + "&date="
+        writer = Writer(category='Article', article_category=category_name, date=self.date)
+        # 기사 url 형식
+        url_format = f'http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1={self.categories.get(category_name)}&date='
         # start_year년 start_month월 ~ end_year의 end_month 날짜까지 기사를 수집합니다.
-        day_urls = self.make_news_page_url(url, self.date['start_year'], self.date['end_year'], self.date['start_month'], self.date['end_month'])
+        target_urls = self.make_news_page_url(url_format, self.date['start_year'], self.date['end_year'], self.date['start_month'], self.date['end_month'])
 
         print(category_name + " Urls are generated")
         print("The crawler starts")
 
-        for URL in day_urls:
-            regex = re.compile("date=(\d+)")
-            news_date = regex.findall(URL)[0]
-
-            request = self.get_url_data(URL)
+        for url in target_urls:
+            request = self.get_url_data(url)
             document = BeautifulSoup(request.content, 'html.parser')
 
             # html - newsflash_body - type06_headline, type06
             # 각 페이지에 있는 기사들 가져오기
-            post_temp = document.select('.newsflash_body .type06_headline li dl')
-            post_temp.extend(document.select('.newsflash_body .type06 li dl'))
+            temp_post = document.select('.newsflash_body .type06_headline li dl')
+            temp_post.extend(document.select('.newsflash_body .type06 li dl'))
             
             # 각 페이지에 있는 기사들의 url 저장
-            post = []
-            for line in post_temp:
-                post.append(line.a.get('href')) # 해당되는 page에서 모든 기사들의 URL을 post 리스트에 넣음
-            del post_temp
+            post_urls = []
+            for line in temp_post:
+                # 해당되는 page에서 모든 기사들의 URL을 post_urls 리스트에 넣음
+                post_urls.append(line.a.get('href'))
+            del temp_post
 
-            for content_url in post:  # 기사 URL
+            for content_url in post_urls:  # 기사 url
                 # 크롤링 대기 시간
                 sleep(0.01)
                 
@@ -138,30 +132,38 @@ class ArticleCrawler(object):
                 try:
                     # 기사 제목 가져옴
                     tag_headline = document_content.find_all('h3', {'id': 'articleTitle'}, {'class': 'tts_head'})
-                    text_headline = ''  # 뉴스 기사 제목 초기화
+                    # 뉴스 기사 제목 초기화
+                    text_headline = ''
                     text_headline = text_headline + ArticleParser.clear_headline(str(tag_headline[0].find_all(text=True)))
-                    if not text_headline:  # 공백일 경우 기사 제외 처리
+                    # 공백일 경우 기사 제외 처리
+                    if not text_headline:
                         continue
 
                     # 기사 본문 가져옴
                     tag_content = document_content.find_all('div', {'id': 'articleBodyContents'})
-                    text_sentence = ''  # 뉴스 기사 본문 초기화
+                    # 뉴스 기사 본문 초기화
+                    text_sentence = ''
                     text_sentence = text_sentence + ArticleParser.clear_content(str(tag_content[0].find_all(text=True)))
-                    if not text_sentence:  # 공백일 경우 기사 제외 처리
+                    # 공백일 경우 기사 제외 처리
+                    if not text_sentence:
                         continue
 
                     # 기사 언론사 가져옴
                     tag_company = document_content.find_all('meta', {'property': 'me2:category1'})
-                    text_company = ''  # 언론사 초기화
-                    text_company = text_company + str(tag_company[0].get('content'))
-                    if not text_company:  # 공백일 경우 기사 제외 처리
-                        continue
-                    #기사 시간대 가져옴
-                    time = re.findall('<span class="t11">(.*)</span>',request_content.text)[0]
-                    # CSV 작성
 
-                    wcsv = writer.get_writer_csv()
-                    wcsv.writerow([time, category_name, text_company, text_headline, text_sentence, content_url])
+                    # 언론사 초기화
+                    text_company = ''
+                    text_company = text_company + str(tag_company[0].get('content'))
+
+                    # 공백일 경우 기사 제외 처리
+                    if not text_company:
+                        continue
+
+                    # 기사 시간대 가져옴
+                    time = re.findall('<span class="t11">(.*)</span>',request_content.text)[0]
+
+                    # CSV 작성
+                    writer.write_row([time, category_name, text_company, text_headline, text_sentence, content_url])
 
                     del time
                     del text_company, text_sentence, text_headline
@@ -169,8 +171,8 @@ class ArticleCrawler(object):
                     del tag_content, tag_headline
                     del request_content, document_content
 
-                except Exception as ex:  # UnicodeEncodeError ..
-                    # wcsv.writerow([ex, content_url])
+                # UnicodeEncodeError
+                except Exception as ex:
                     del request_content, document_content
                     pass
         writer.close()
@@ -184,6 +186,6 @@ class ArticleCrawler(object):
 
 if __name__ == "__main__":
     Crawler = ArticleCrawler()
-    Crawler.set_category('생활문화','경제')
-    Crawler.set_date_range(2017, 4, 2018, 1)
+    Crawler.set_category('생활문화')
+    Crawler.set_date_range(2018, 1, 2018, 2)
     Crawler.start()
--- korea_news_crawler/sportcrawler.py
+++ korea_news_crawler/sportcrawler.py
@@ -1,28 +1,28 @@
-from bs4 import BeautifulSoup
-from time import sleep
 import calendar
 import csv
 import requests
 import re
 import json
-from exceptions import *
+from bs4 import BeautifulSoup
+from time import sleep
 from multiprocessing import Process
+from korea_news_crawler.exceptions import *
+from korea_news_crawler.writer import Writer
+
 
 class SportCrawler:
     def __init__(self):
-        self.category = {'한국야구': "kbaseball",'해외야구': "wbaseball",'해외축구' : "wfootball",
+        self.category = {'한국야구': "kbaseball",'해외야구': "wbaseball", '해외축구': "wfootball",
                          '한국축구': "kfootball", '농구': "basketball", '배구': "volleyball", '일반 스포츠': "general", 'e스포츠': "esports"}
         self.selected_category = []
-        self.selected_urlcategory=[]
-        self.date = {'startyear': 0,'startmonth':0, 'endyear': 0, 'endmonth': 0}
+        self.selected_url_category = []
+        self.date = {'start_year': 0, 'start_month': 0, 'end_year': 0, 'end_month': 0}
 
-    def javascript_totalpage(self, url):
-
-        totalpage_url = url +"&page=10000"
-        request_content = requests.get(totalpage_url,headers={'User-Agent':'Mozilla/5.0'})
-        pagenumber = re.findall('\"totalPages\":(.*)}',request_content.text)
-        return int(pagenumber[0])
-
+    def get_total_page(self, url):
+        totalpage_url = f'{url}&page=10000'
+        request_content = requests.get(totalpage_url,headers={'User-Agent': 'Mozilla/5.0'})
+        page_number = re.findall('\"totalPages\":(.*)}', request_content.text)
+        return int(page_number[0])
 
     def content(self, html_document, url_label):
         content_match = []
@@ -36,117 +36,127 @@ class SportCrawler:
             content_match.append(maked_url)
         return content_match
 
-    def Clearcontent(self, text):
+    def clear_content(self, text):
         remove_special = re.sub('[\{\}\[\]\/?,;:|\)*~`!^\-_+<>@\#$%&n▲▶◆◀■\\\=\(\'\"]', '', text)
         remove_author = re.sub('\w\w\w 기자', '', remove_special)
-        remove_flasherror = re.sub(
-            '본문 내용|TV플레이어| 동영상 뉴스|flash 오류를 우회하기 위한 함수 추가fuctio flashremoveCallback|tt|t|앵커 멘트|xa0', '', remove_author)
-        remove_strip = remove_flasherror.strip().replace('   ', '')  # 공백 에러 삭제
-        reverse_content = ''.join(reversed(remove_strip))  # 기사 내용을 reverse 한다.
+        remove_flash_error = re.sub('본문 내용|TV플레이어| 동영상 뉴스|flash 오류를 우회하기 위한 함수 추가fuctio flashremoveCallback|tt|t|앵커 멘트|xa0', '', remove_author)
+        # 공백 에러 삭제
+        remove_strip = remove_flash_error.strip().replace('   ', '')
+        # 기사 내용을 reverse 한다.
+        reverse_content = ''.join(reversed(remove_strip))
         cleared_content = ''
         for i in range(0, len(remove_strip)):
-            if reverse_content[
-               i:i + 2] == '.다':  # 기사가 reverse 되었기에  ".다"로 기사가 마무리 되므로, 이를 탐색하여 불필요한 정보를 모두 지운다.
-
+            # 기사가 reverse 되었기에  ".다"로 기사가 마무리 되므로, 이를 탐색하여 불필요한 정보를 모두 지운다.
+            if reverse_content[i:i + 2] == '.다':
                 cleared_content = ''.join(reversed(reverse_content[i:]))
                 break
-        cleared_content = re.sub('if deployPhase(.*)displayRMCPlayer ','',cleared_content)
+        cleared_content = re.sub('if deployPhase(.*)displayRMCPlayer ', '', cleared_content)
         return cleared_content
 
-    def Clearheadline(self, text):
+    def clear_headline(self, text):
         first = re.sub('[\{\}\[\]\/?,;:|\)*~`!^\-_+<>@\#$%&n▲▶◆◀■\\\=\(\'\"]', '', text)
         return first
 
-    def Make_url(self, URL, startyear, lastyear, startmonth, lastmonth):
-        Maked_url = []
-        final_startmonth = startmonth
-        final_lastmonth = lastmonth
-        for year in range(startyear, lastyear + 1):
-            if year != lastyear:
-                startmonth = 1
-                lastmonth = 12
-            else:
-                startmonth = final_startmonth
-                lastmonth = final_lastmonth
-            for Month in range(startmonth, lastmonth + 1):
-                for Month_Day in range(1, calendar.monthrange(year, Month)[1] + 1):
-                    url = URL
-                    if len(str(Month)) == 1:
-                        Month = "0" + str(Month)
-                    if len(str(Month_Day)) == 1:
-                        Month_Day = "0" + str(Month_Day)
-                    url = f'{url}{year}{Month}{Month_Day}'
-                    final_url = url  # page 날짜 정보만 있고 page 정보가 없는 url 임시 저장
+    def make_sport_page_url(self, url, start_year, last_year, start_month, last_month):
+        urls = []
+        for year in range(start_year, last_year + 1):
+            target_start_month = start_month
+            target_last_month = last_month
 
-                    totalpage = self.javascript_totalpage(url)  # TotalPage 확인
-                    for page in range(1, totalpage + 1):
-                        url = final_url  # url page 초기화
+            if year != last_year:
+                target_start_month = 1
+                target_last_month = 12
+            else:
+                target_start_month = start_month
+                target_last_month = last_month
+
+            for month in range(target_start_month, target_last_month + 1):
+                for day in range(1, calendar.monthrange(year, month)[1] + 1):
+                    url = url
+                    if len(str(month)) == 1:
+                        month = "0" + str(month)
+                    if len(str(day)) == 1:
+                        day = "0" + str(day)
+                    url = f'{url}{year}{month}{day}'
+                    # page 날짜 정보만 있고 page 정보가 없는 url 임시 저장
+                    final_url = url
+
+                    # TotalPage 확인
+                    total_page = self.get_total_page(url)
+                    for page in range(1, total_page + 1):
+                        # url page 초기화
+                        url = final_url
                         url = f'{url}&page={page}'
-                        Maked_url.append(url)  # [[page1,page2,page3 ....]
-        return Maked_url
-    def crawling(self, category_name):
-        Url_category = []
-        Category = []
-        Category.append(category_name)
-        Url_category.append(self.category[category_name])
-        titlescript = []
+                        # [[page1,page2,page3 ....]
+                        urls.append(url)
+        return urls
 
-        officename_script = []
-        completed_content_match = []
-        timescript = []
-        for url_label in Url_category:  # URL 카테고리. Multiprocessing시 어차피 1번 도는거라 refactoring할 필요 있어보임
-            category = Category[Url_category.index(url_label)]  # URL 인덱스와 Category 인덱스가 일치할 경우 그 값도 일치
+    def crawling(self, category_name):
+        writer = Writer(category='Sport', article_category=category_name, date=self.date)
+
+        url_category = [self.category[category_name]]
+        category = [category_name]
+
+        title_script = []
+        office_name_script = []
+        time_script = []
+        matched_content = []
+
+        # URL 카테고리. Multiprocessing시 어차피 1번 도는거라 refactoring할 필요 있어보임
+        for url_label in url_category:
+            # URL 인덱스와 category 인덱스가 일치할 경우 그 값도 일치
+            category = category[url_category.index(url_label)]
             url = f'https://sports.news.naver.com/{url_label}/news/list.nhn?isphoto=N&view=photo&date='
-            final_urlday = self.Make_url(url,self.date['startyear'],
-                                                self.date['endyear'], self.date['startmonth'], self.date['endmonth'])
+            final_url_day = self.make_sport_page_url(url, self.date['start_year'],
+                                                    self.date['end_year'], self.date['start_month'], self.date['end_month'])
             print("succeed making url")
-            if len(str(self.date['startmonth'])) == 2:
-                startmonth = str(self.date['startmonth'])
+            if len(str(self.date['start_month'])) == 2:
+                start_month = str(self.date['start_month'])
             else:
-                startmonth = '0'+str(self.date['startmonth'])
-            if len(str(self.date['endmonth']))==2:
-                endmonth=str(self.date['endmonth'])
+                start_month = '0' + str(self.date['start_month'])
+
+            if len(str(self.date['end_month'])) == 2:
+                end_month = str(self.date['end_month'])
             else:
-                endmonth = '0'+str(self.date['endmonth'])
-            file = open("Sport_" + category + "_"+str(self.date['startyear'])+str(startmonth)
-                        +"_"+str(self.date['endyear'])+str(endmonth)+".csv", 'w', encoding='euc-kr', newline='')
-            #위의 경우 self.date['endyear']가 f string으로 사용이 안됩니다.
-            wcsv = csv.writer(file)
-            hefscript2 = [] #이는 크롤링이 아닌 csv에 사용
-            for list_page in final_urlday:  # Category Year Month Data Page 처리 된 URL
+                end_month = '0' + str(self.date['end_month'])
+
+            # 이는 크롤링이 아닌 csv에 사용
+            hefscript2 = []
+            # category Year Month Data Page 처리 된 URL
+            for list_page in final_url_day:
                 # 제목 / URL
                 request_content = requests.get(list_page, headers={'User-Agent': 'Mozilla/5.0'})
                 content_dict = json.loads(request_content.text)
-                hefscript = [] #이는 크롤링에 사용
+                # 이는 크롤링에 사용
+                hefscript = []
                 for contents in content_dict["list"]:
                     oid = contents['oid']
                     aid = contents['aid']
-                    titlescript.append(contents['title'])
-                    timescript.append(contents['datetime'])
+                    title_script.append(contents['title'])
+                    time_script.append(contents['datetime'])
                     hefscript.append("https://sports.news.naver.com/news.nhn?oid=" + oid + "&aid=" + aid)
                     hefscript2.append("https://sports.news.naver.com/news.nhn?oid=" + oid + "&aid=" + aid)
-                    officename_script.append(contents['officeName'])
+                    office_name_script.append(contents['officeName'])
                 # 본문
                 # content page 기반하여 본문을 하면 된다. text_sentence에 본문을 넣고 Clearcontent진행 후 completed_conten_match에 append해주면 된다.
                 # 추가적으로 pass_match에 언론사를 집어넣으면 된다.
 
                 for content_page in hefscript:
-
                     sleep(0.01)
                     content_request_content = requests.get(content_page, headers={'User-Agent': 'Mozilla/5.0'})
                     content_document_content = BeautifulSoup(content_request_content.content, 'html.parser')
-                    content_Tag_content = content_document_content.find_all('div', {'class': 'news_end'},
-                                                                            {'id': 'newsEndContents'})
-                    text_sentence = ''  # 뉴스 기사 본문 내용 초기화
+                    content_tag_content = content_document_content.find_all('div', {'class': 'news_end'}, {'id': 'newsEndContents'})
+                    # 뉴스 기사 본문 내용 초기화
+                    text_sentence = ''
 
                     try:
-                        text_sentence = text_sentence + str(content_Tag_content[0].find_all(text=True))
-                        completed_content_match.append(self.Clearcontent(text_sentence))
+                        text_sentence = text_sentence + str(content_tag_content[0].find_all(text=True))
+                        matched_content.append(self.clear_content(text_sentence))
                     except:
                         pass
 
             # Csv 작성
-            for csv_timeline, csv_headline, csv_content, csv_press, csv_url in zip(timescript,titlescript, completed_content_match, officename_script,hefscript2):
+            for csv_timeline, csv_headline, csv_content, csv_press, csv_url in zip(time_script, title_script, matched_content, office_name_script, hefscript2):
                 try:
                     if not csv_timeline:
                         continue
@@ -158,11 +168,11 @@ class SportCrawler:
                         continue
                     if not csv_url:
                         continue
-                    wcsv.writerow([csv_timeline, self.Clearheadline(csv_headline), csv_content, csv_press, category,csv_url])
+                    writer.write_row([csv_timeline, self.clear_headline(csv_headline), csv_content, csv_press, category, csv_url])
                 except:
                     pass
 
-            file.close()
+            writer.close()
 
     def set_category(self, *args):
         for key in args:
@@ -170,22 +180,24 @@ class SportCrawler:
                 raise InvalidCategory(key)
         self.selected_category = args
         for selected in self.selected_category:
-            self.selected_urlcategory.append(self.category[selected])
+            self.selected_url_category.append(self.category[selected])
+
     def start(self):
         # MultiProcess 크롤링 시작
         for category_name in self.selected_category:
             proc = Process(target=self.crawling, args=(category_name,))
             proc.start()
-    def set_date_range(self,startyear,startmonth,endyear,endmonth):
-        self.date['startyear'] = startyear
-        self.date['startmonth'] = startmonth
-        self.date['endyear'] = endyear
-        self.date['endmonth'] = endmonth
+
+    def set_date_range(self, start_year, start_month, end_year, end_month):
+        self.date['start_year'] = start_year
+        self.date['start_month'] = start_month
+        self.date['end_year'] = end_year
+        self.date['end_month'] = end_month
 
 
 # Main
 if __name__ == "__main__":
     Spt_crawler = SportCrawler()
-    Spt_crawler.set_category('한국야구','한국축구')
-    Spt_crawler.set_date_range(2020,12,2020,12)
+    Spt_crawler.set_category('한국야구')
+    Spt_crawler.set_date_range(2020, 12, 2020, 12)
     Spt_crawler.start()
