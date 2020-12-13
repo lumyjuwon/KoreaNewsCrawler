8338dc1a14580bdb2be78049a1c7fa1176920f53
slslslrhfem <skskskrhfem@naver.com>
2020-12-14 03:07:35 +0900

    스포츠크롤러 이슈 해결 및 writer encoder 수정
    스포츠크롤러의 self.clear_headline 함수가 self.ClearHeadline으로 쓰여있어 아예 크롤링이 되지 않던 오류를 해결했으며, 데이터를 한번에 모아두고 csv에 쓰는 방식이 아닌 뉴스마다 csv에 쓰는 방식으로 바꾸었습니다.
    또한 windows10에서의 인코딩 방식이 달라 생기는 이슈때문에 writer.py를 수정합니다.

--- korea_news_crawler/sports_crawler_sample.py
+++ korea_news_crawler/sports_crawler_sample.py
@@ -3,5 +3,5 @@ from korea_news_crawler.sportcrawler import SportCrawler
 if __name__ == "__main__":
     Spt_crawler = SportCrawler()
     Spt_crawler.set_category('한국야구', '한국축구')
-    Spt_crawler.set_date_range(2020, 12, 2020, 12)
+    Spt_crawler.set_date_range(2020, 11, 2020, 11)
     Spt_crawler.start()
