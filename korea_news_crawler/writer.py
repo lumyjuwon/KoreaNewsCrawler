7f607b204987d9a9a16d6d212532d110e5ee603f
geun9716 <crintop@gmail.com>
2022-03-22 18:25:44 +0900

    Add Writer day Field

--- korea_news_crawler/writer.py
+++ korea_news_crawler/writer.py
@@ -7,9 +7,10 @@ class Writer(object):
     def __init__(self, category, article_category, date):
         self.start_year = date['start_year']
         self.start_month = f'0{date["start_month"]}' if len(str(date['start_month'])) == 1 else str(date['start_month'])
+        self.start_day = f'0{date["start_day"]}' if len(str(date['start_day'])) == 1 else str(date['start_day'])
         self.end_year = date['end_year']
         self.end_month = f'0{date["end_month"]}' if len(str(date['end_month'])) == 1 else str(date['end_month'])
-
+        self.end_day = f'0{date["end_day"]}' if len(str(date['end_day'])) == 1 else str(date['end_day'])
         self.file = None
         self.initialize_file(category, article_category)
 
@@ -20,7 +21,7 @@ class Writer(object):
         if os.path.exists(output_path) is not True:
             os.mkdir(output_path)
 
-        file_name = f'{output_path}/{category}_{article_category}_{self.start_year}{self.start_month}_{self.end_year}{self.end_month}.csv'
+        file_name = f'{output_path}/{category}_{article_category}_{self.start_year}{self.start_month}{self.start_day}_{self.end_year}{self.end_month}{self.end_day}.csv'
         if os.path.isfile(file_name):
             raise ExistFile(file_name)
 
